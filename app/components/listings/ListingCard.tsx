fc1eecbe7e64243bdfc4b20af36598c7fd89a0a5
sagnikghosh1999 <saggywebd@gmail.com>
2024-05-14 22:22:43 +0530

    bug fix

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -83,7 +83,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
           {reservationDate || data.category}
         </div>
         <div className="flex flex-row items-center gap-1">
-          <div className="font-semibold">₹{price}</div>
+          <div className="font-semibold">₹{price.toLocaleString()}</div>
           {!reservation && <div className="font-light ">night</div>}
         </div>
         {onAction && actionLabel && (
